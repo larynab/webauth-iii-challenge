08eac37653568e4e2f99d5651f4b4968986f45b8
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-04-04 10:45:15 -0700

    verify-token-authorization-passed

--- database/authentication/restricted-middleware.js
+++ database/authentication/restricted-middleware.js
@@ -1,8 +1,8 @@
 const bcrypt = require("bcryptjs");
 //JSON web token
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 //import secret
-const secret = require('./secrets').jwtSecret;
+const secrets = require("./secrets");
 
 const Users = require("../users/users-model.js");
 
@@ -10,28 +10,37 @@ module.exports = (req, res, next) => {
   //username-password not needed anymore, because of token
   // const { username, password } = req.headers;
   const token = req.headers.authorization;
-  if(token) {
-    //VERIFY token
-    jwt.verify(token, secret,)
+  if (token) {
+    //VERIFY token with error callback
+    jwt.verify(token, secrets.jwtSecret, (error, decodedToken) => {
+      if (error) {
+        //token not valid
+        res.status(401).json({ message: "Invalid Credentials" });
+      } else {
+        //token valid
+        //req.decodedJwt = decodedToken;
+
+        next();
+      }
+    });
   } else {
     res.status(401).json({ message: "access denied" });
   }
-
-
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: "Invalid Credentials" });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: "Ran into an unexpected error" });
-      });
-  } else {
-    res.status(400).json({ message: "No credentials provided" });
-  }
+  //token takes over
+  // if (username && password) {
+  //   Users.findBy({ username })
+  //     .first()
+  //     .then(user => {
+  //       if (user && bcrypt.compareSync(password, user.password)) {
+  //         next();
+  //       } else {
+  //         res.status(401).json({ message: "Invalid Credentials" });
+  //       }
+  //     })
+  //     .catch(error => {
+  //       res.status(500).json({ message: "Ran into an unexpected error" });
+  //     });
+  // } else {
+  //   res.status(400).json({ message: "No credentials provided" });
+  // }
 };
