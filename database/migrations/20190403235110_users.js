27e48fea11b79b70700fb114a7b328811fdc5094
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-04-03 23:53:33 -0700

    knex-sqlite3-created-and-migrated

---
+++ database/migrations/20190403235110_users.js
@@ -0,0 +1,17 @@
+exports.up = function(knex) {
+  //table creation
+  return knex.schema.createTable("users", users => {
+    users.increments();
+
+    users
+      .string("username", 128)
+      .notNullable()
+      .unique();
+    users.string("password", 128).notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  //table rollback
+  return knex.schema.dropTableIfExists("users");
+};
